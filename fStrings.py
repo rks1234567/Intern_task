72e315a8dc352805bfd8fdf77d41b851565ce400
rks1234567 <rohitkumar.sharma@srsconsultinginc.com>
2026-05-25 01:47:25 -0700

    I am adding this file

---
+++ fStrings.py
@@ -0,0 +1,9 @@
+# String Formatting
+# 1st way is using format function
+a = 5
+b = 10
+sum = a+b
+print("Sum of {} and {} is: {} ".format(a,b, sum))
+
+# 2nd way is using f-strings
+print(f"Sum of {a} and {b} is: {sum}")
