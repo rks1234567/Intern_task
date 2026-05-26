8db9156f9eca3310ee3db45603eb45a84fd55829
rks1234567 <rohitkumar.sharma@srsconsultinginc.com>
2026-05-25 21:37:00 -0700

    Day2 Task

---
+++ Day2Task/Logicaloperators.py
@@ -0,0 +1,11 @@
+""" we have simply three logical operators 
+1st is NOT operator
+2nd is OR operator
+3rd is AND operator
+"""
+
+print(not(3>8)) # true result because of not.  it converts true to false or false to true.
+
+print((3>2) or (5>6)) # if any of the condition is true it gave true result.
+
+print((5>1) and (3>6)) # if any of the condition is false it gives false result.
