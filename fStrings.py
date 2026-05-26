cd327ebc4b154a21dd2e4379db659a1f82fa3336
rks1234567 <rohitkumar.sharma@srsconsultinginc.com>
2026-05-25 21:53:58 -0700

    Delete fStrings.py

--- fStrings.py
+++
@@ -1,9 +0,0 @@
-# String Formatting
-# 1st way is using format function
-a = 5
-b = 10
-sum = a+b
-print("Sum of {} and {} is: {} ".format(a,b, sum))
-
-# 2nd way is using f-strings
-print(f"Sum of {a} and {b} is: {sum}")
