1c3f75a40dc545a9201ffd29950d11d55900508e
rks1234567 <rohitkumar.sharma@srsconsultinginc.com>
2026-05-25 23:40:37 -0700

    Day2 Task

--- slicingStrings.py
+++
@@ -1,8 +0,0 @@
-str = "python"
-print(str[2:4]) # so this will give me th as output
-# in slicing str[starting index : ending index]
-# but ending index is excluded in this
-
-# negative index starts from right side so if want to print the same th then
-print(str[-4:-2])
-# so this th is called substring
