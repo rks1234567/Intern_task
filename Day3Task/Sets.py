2476258eb2307cad668c367bd134cc36fac607ae
rks1234567 <rohitkumar.sharma@srsconsultinginc.com>
2026-05-26 23:26:36 -0700

    Hi

--- Day3Task/Sets.py
+++ Day3Task/Sets.py
@@ -21,3 +21,15 @@ print(type(empty_set)) # it's type is <class 'dict'>
 # so we can create empty set as 
 empty_set = set()
 print(type(empty_set))  #in this way i can create an empty_set
+
+
+# Set methods
+s1 = {1,2,3}
+s2 = {5,6}
+s1.add(4) # adds a value 
+s1.remove(2) # removes a value
+# s1.clear() # clears the set
+s1.pop() # removes a random value
+s1.union(s2) # return new union
+
+print(s1)
