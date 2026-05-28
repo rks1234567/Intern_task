2d8dd440a51c16d205e041a46aeec766171aec06
rks1234567 <rohitkumar.sharma@srsconsultinginc.com>
2026-05-27 22:40:13 -0700

    Day4 Task

--- Day4Task/test1.py
+++ Day4Task/test1.py
@@ -1,4 +1,6 @@
-f = open("sample.txt", "r")
+with open("Day4Task/sample.txt", "r") as f:
 
-data = f.read()
-print(data)
+    # data = f.read()  # it reads all the text present inside the text file.
+    data = f.readline() # it reads line by line
+    print(data)
+
