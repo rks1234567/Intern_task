0b7086e417cf7d5167bf20ceb8ec01bee2a030bd
rks1234567 <rohitkumar.sharma@srsconsultinginc.com>
2026-05-27 22:59:27 -0700

    Day 4 Task

--- Day4Task/test1.py
+++ Day4Task/test1.py
@@ -1,6 +1,14 @@
-with open("Day4Task/sample.txt", "r") as f:
+# with open("Day4Task/sample.txt", "r") as f:
 
     # data = f.read()  # it reads all the text present inside the text file.
-    data = f.readline() # it reads line by line
-    print(data)
+    # data = f.readline() # it reads line by line
+    # print(data)
+
+
+# Also if we want to overwrite changes in our text file then we can use
+with open(r"C:\Users\srs\Desktop\Git Demo\Intern_task\Day4Task\sample.txt", "w") as f:
+    f.write("Hello,this write method overwrites the text in sample.text file")
+
+f.close()
+
 
