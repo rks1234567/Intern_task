0a48121e1cc284ef57dac4532cd2fac0bf906af1
rks1234567 <rohitkumar.sharma@srsconsultinginc.com>
2026-05-30 00:23:47 -0700

    Added Day 6 Gemini API task

---
+++ Day6Task/main.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+env_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(env_path)
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Explain machine learning in exactly 50 words."
+)
+
+print(response.text)
+
+usage = response.usage_metadata
+
+print("\nToken Usage")
+print("Prompt Tokens:", usage.prompt_token_count)
+print("Response Tokens:", usage.candidates_token_count)
+print("Total Tokens:", usage.total_token_count)
+
+print("\nFull Usage Metadata:")
+print(usage)
